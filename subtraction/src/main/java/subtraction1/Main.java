ee3ac048bb3861868fffab04ff0348d1cad26999
ravikumark1150 <ravikumark11509@outlook.com>
2024-07-20 16:16:38 +0530

    the code has ben redy

--- subtraction/src/main/java/subtraction1/Main.java
+++ subtraction/src/main/java/subtraction1/Main.java
@@ -3,7 +3,10 @@ package subtraction1;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Sub sb = new Sub("ravi","nt.11");
+	System.out.println("Customer Id :"+sb.getId());
+	System.out.println("Customer Name :"+sb.getName());
+		
 
 	}
 
--- subtraction/src/main/java/subtraction1/Sub.java
+++ subtraction/src/main/java/subtraction1/Sub.java
@@ -7,5 +7,21 @@ public class Sub {
 		this.name=name;
 		this.id=id;
 	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+public String	toString() {
+	
+	return name+"\t"+id;
+}
 	
 }
